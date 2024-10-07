899ec0b977e88a1c561ef4514d8209bc739548ec
Andromeda Canete-Borys <andycan@student.ubc.ca>
2024-10-07 16:54:48 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,20 @@
+FILEPATH = "/Users/ams/Coding2024/App1/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file  and return the list of to do items.
+    """
+    with open (filepath,'r') as file_local   :
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the todo items list in the text file """
+    with open(filepath,'w') as file:
+        file.writelines(todos_arg)
+
+# Below will only be executed if this script is executed directly
+if __name__ == "__main__":
+    print ("Hello")
+    print(get_todos())
